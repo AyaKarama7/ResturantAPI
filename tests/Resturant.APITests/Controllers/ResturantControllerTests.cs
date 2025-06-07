2bc58a04941413d3eb9d92d875707b508740746e
Aya <ayakarama469@gmail.com>
2025-06-07 03:31:03 +0300

    [Edit] Restructure solution to src and tests

---
+++ tests/Resturant.APITests/Controllers/ResturantControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using FluentAssertions;
+namespace Resturant.API.Controllers.Tests;
+public class ResturantControllerTests: IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    public ResturantControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        #region what is _factory
+        // This will create a test server for the API, 
+        // allowing us to send HTTP requests to it.
+        // This is useful for integration testing.
+        // // The Program class is the entry point of the API,
+        // // and it will be used to configure the test server.
+        // // // The factory will create a new instance of the Program class
+        // // // and use it to configure the test server.
+        // // // // The factory will also create a new instance of the HttpClient class,
+        // // // // which will be used to send HTTP requests to the test server 
+        #endregion
+    }
+    [Fact()]
+    public async Task GetAllTest_ForValidRequest_Return200Ok()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        // Act
+        var response = await client.GetAsync("/api/resturant");
+        // Assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+    [Fact()]
+    public async Task GetByIdTest_ForValidRequest_Return200Ok()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        // Act
+        var response = await client.GetAsync("/api/resturant/4");
+        // Assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+    [Fact()]
+    public async Task GetByIdTest_ForInvalidRequest_Return404NotFound()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        // Act
+        var response = await client.GetAsync("/api/resturant/999");
+        // Assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
+}
---
+++ tests/Resturant.ApplicationTests/User/UserContextTests.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+using Resturant.Domain.Constants;
+using FluentAssertions;
+namespace Resturant.Application.User.Tests;
+public class UserContextTests
+{
+    [Fact()]
+    public void GetCurrentUserTest_WithAuthentcatedUser_ShouldReturnCurrentUser()
+    {
+        var context = new HttpContextAccessor();
+        var httpContextAccessor = new Mock<IHttpContextAccessor>();
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.NameIdentifier, "123"),
+            new Claim(ClaimTypes.Email,"testuser@gmail.com"),
+            new Claim(ClaimTypes.Role, UserRoles.Admin),
+            new Claim(ClaimTypes.Role, UserRoles.User)
+        };
+        //create a user object that holds identity information and claims.
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims,"TestAuth"));
+        //assign the user to the HttpContext of the mock IHttpContextAccessor.
+        httpContextAccessor.Setup(x => x.HttpContext).Returns(new DefaultHttpContext { User = user });
+        var userContext = new UserContext(httpContextAccessor.Object);
+        //act
+        var currentUser = userContext.GetCurrentUser();
+        //assert
+        currentUser.Should().NotBeNull();
+        currentUser!.Id.Should().Be("123");
+        currentUser!.Email.Should().Be("testuser@gmail.com");
+        currentUser!.Roles.Should().Contain(UserRoles.Admin);
+        currentUser!.Roles.Should().Contain(UserRoles.User);
+    }
+    [Fact()]
+    public void GetCurrentUserTest_WithUnauthenticatedUser_ShouldReturnNull()
+    {
+        var httpContextAccessor = new Mock<IHttpContextAccessor>();
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.NameIdentifier, "123"),
+            new Claim(ClaimTypes.Email,"testuser@gmail.com"),
+            new Claim(ClaimTypes.Role, UserRoles.Admin),
+            new Claim(ClaimTypes.Role, UserRoles.User)
+        };
+        //create a user object that holds identity information and claims.
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims));
+        //assign the user to the HttpContext of the mock IHttpContextAccessor.
+        httpContextAccessor.Setup(x => x.HttpContext).Returns(new DefaultHttpContext { User = user });
+        var userContext = new UserContext(httpContextAccessor.Object);
+        //act
+        var currentUser = userContext.GetCurrentUser();
+        //assert
+        currentUser.Should().BeNull();
+    }
+    [Fact()]
+    public void GetCurrentUserTest_WithNoUser_ShouldThrowInvalidOperationException()
+    {
+        var httpContextAccessor = new Mock<IHttpContextAccessor>();
+        //set up the mock to return null for HttpContext
+        httpContextAccessor.Setup(x => x.HttpContext).Returns((HttpContext?)null);
+        var userContext = new UserContext(httpContextAccessor.Object);
+        //act
+        Action act = () => userContext.GetCurrentUser();
+        //assert
+        act.Should().Throw<InvalidOperationException>().WithMessage("User context is not present");
+    }
+    [Fact()]
+    public void GetCurrentUserTest_WithNoIdentity_ShouldThrowInvalidOperationException()
+    {
+        var httpContextAccessor = new Mock<IHttpContextAccessor>();
+        //create a user object that holds identity information and claims.
+        var user = new ClaimsPrincipal(new ClaimsIdentity());
+        //assign the user to the HttpContext of the mock IHttpContextAccessor.
+        httpContextAccessor.Setup(x => x.HttpContext).Returns(new DefaultHttpContext { User = user });
+        var userContext = new UserContext(httpContextAccessor.Object);
+        //act
+        var currentUser = userContext.GetCurrentUser();
+        //assert
+        currentUser.Should().BeNull();
+    }
+}
